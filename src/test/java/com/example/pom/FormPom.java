d5a66aa9b7f5b0b8a5a7907e190d36f95d763a2b
vasylec <cozma.vasyle@gmail.com>
2026-02-25 10:08:32 +0200

    commit

--- src/test/java/com/example/pom/FormPom.java
+++ src/test/java/com/example/pom/FormPom.java
@@ -1,18 +1,24 @@
 package com.example.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.example.utils.Utils;
 
 public class FormPom {
     static public WebDriver driver;
+    static public JavascriptExecutor js;
 
     @FindBy(xpath = "//*[text()='Forms']")
     WebElement forms;
 
-    @FindBy(xpath = "/html/body/div/div[1]/div/div/div[1]/div/div/div[2]/div/ul/li/a/span")
+    @FindBy(xpath = "/html/body/div/div/div/div/div[1]/div/div/div[2]/div/ul/li/a/span")
     WebElement practiceForms;
 
     @FindBy(xpath = "//*[@id='firstName']")
@@ -24,42 +30,149 @@ public class FormPom {
     @FindBy(xpath = "//*[@id=\"userEmail\"]")
     WebElement email;
 
+    @FindBy(xpath = "//*[@id=\"userNumber\"]")
+    WebElement number;
+
+    @FindBy(xpath = "//*[@id=\"dateOfBirthInput\"]")
+    WebElement dateOfBirth;
+
+    @FindBy(xpath = "//*[@id=\"subjectsInput\"]")
+    WebElement subjectsInput;
+
+    @FindBy(xpath = "//*[@id=\"state\"]")
+    WebElement state;
+
+    @FindBy(xpath = "//*[@id=\"city\"]")
+    WebElement city;
+
+    @FindBy(xpath = "// *[@id=\"submit\"]")
+    WebElement submitButton;
+
     public FormPom(WebDriver driverParam) {
         driver = driverParam;
+        js = (JavascriptExecutor) driverParam;
         PageFactory.initElements(driver, this);
     }
 
-    private void scrollToElement(WebElement element) {
-        ((JavascriptExecutor) driver)
-                .executeScript("arguments[0].scrollIntoView({block: 'center'});", element);
-    }
-
     public void clickForms() {
+
+        // Utils.explicitWait(driver, ExpectedConditions.visibilityOf(forms),
+        // 1);
+
         scrollToElement(forms);
+
         forms.click();
     }
 
     public void clickPracticeForms() {
+        // Utils.explicitWait(driver, ExpectedConditions.visibilityOf(practiceForms),
+        // 20);
         scrollToElement(practiceForms);
+
         practiceForms.click();
     }
 
     public void setFirstName(String firstNameParam) {
-        scrollToElement(firstName);
+        Utils.explicitWait(driver, ExpectedConditions.visibilityOf(practiceForms),
+                10);
+
         firstName.clear();
         firstName.sendKeys(firstNameParam);
     }
 
     public void setLastName(String lastNameParams) {
-        scrollToElement(lastName);
+
         lastName.clear();
         lastName.sendKeys(lastNameParams);
     }
 
     public void setEmail(String userEmailParams) {
-        scrollToElement(email);
+
         email.clear();
         email.sendKeys(userEmailParams);
+    }
+
+    public void setNumber(String numberParam) {
+        scrollToElement(practiceForms);
+        number.clear();
+        number.sendKeys(numberParam);
+    }
+
+    public void setDateOfBirth(String dateParam) {
+        scrollToElement(practiceForms);
+        dateOfBirth.sendKeys(Keys.CONTROL, "a");
+        dateOfBirth.sendKeys(dateParam);
+        dateOfBirth.sendKeys(Keys.ENTER);
+    }
+
+    public void setSubject(String subjectParam) {
+        scrollToElement(practiceForms);
+        subjectsInput.sendKeys(subjectParam);
+        subjectsInput.sendKeys(Keys.ENTER);
+    }
+
+    public void setHobbies(String hobbyParam) {
+        scrollToElement(practiceForms);
+        WebElement ddState = driver
+                .findElement(By.xpath("//*[@id='hobbiesWrapper']//label[text()='" + hobbyParam + "']/../input"));
+        ddState.sendKeys(" ");
+    }
+
+    public void setState(String stateParam) {
+        scrollToElement(practiceForms);
+        state.click();
+        WebElement optionState = state.findElement(By.xpath(".//*[text()='" + stateParam + "']"));
+        optionState.click();
+
+    }
+
+    public void setCity(String cityParam) {
+        scrollToElement(practiceForms);
+        city.click();
+        WebElement optionCity = city.findElement(By.xpath(".//*[text()='" + cityParam + "']"));
+        optionCity.click();
+    }
+
+    public void setGender(String genderParam) {
+        scrollToElement(practiceForms);
+        WebElement gender = driver
+                .findElement(By.xpath("//*[@id='genterWrapper']//label[text()='" + genderParam + "']"));
+        gender.click();
+    }
+
+    public void clickSubmit() {
+        scrollToElement(submitButton);
+        submitButton.click();
+    }
+
+    public String getTableData(String labelParam) {
+        String value = driver.findElement(By.xpath("//table//*[text()='" + labelParam + "']/../*[2]")).getText();
+        return value;
+    }
+
+    public void scrollToElement(WebElement element) {
+
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+
+    }
+
+    public void closeAdvert() {
+
+        try {
+            js.executeScript(
+                    "var elem = document.evaluate(\"//*[@id='adplus-anchor']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;"
+                            +
+                            "elem.parentNode.removeChild(elem);");
+        } catch (Exception ignored) {
+        }
+        try {
+            js.executeScript(
+                    "var elem = document.evaluate(\"//footer\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;"
+                            +
+                            "elem.parentNode.removeChild(elem);");
+        } catch (Exception ignored) {
+        }
+
     }
 
     public void pause(int ms) {
--- src/test/java/com/example/testng/FormTest.java
+++ src/test/java/com/example/testng/FormTest.java
@@ -1,7 +1,9 @@
 package com.example.testng;
 
-import org.openqa.selenium.JavascriptExecutor;
+import java.net.MalformedURLException;
+
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,33 +19,57 @@ public class FormTest {
     static public String FIRST_NAME = "Cozma";
     static public String LAST_NAME = "Vasile";
     static public String EMAIL = "vcozma06@gmail.com";
+    static public String GENDER = "Male";
+    static public String NUMBER = "0987654321";
+    static public String DATE = "14 Jan 2006";
+    static public String SUBJECT = "Maths";
+    static public String HOBBY = "Sports";
+    static public String STATE = "Rajasthan";
+    static public String CITY = "Jaipur";
 
     @BeforeMethod
-    public void beforeMethod() {
+    public void beforeMethod() throws MalformedURLException {
+        // driver = Driver.getRemoteDriver();
         driver = Driver.getAutoLocalDriver();
         driver.manage().window().maximize();
     }
 
-    @Test
+    @SuppressWarnings("null")
+    @Test(invocationCount = 1)
     public void formTest() {
         // demoqa.com
-        System.out.println("---START TEST---");
+        System.out.println("\n---START TEST---\n");
         driver.get(URL);
+
         FormPom formPom = new FormPom(driver);
 
         formPom.pause(1000);
 
         formPom.clickForms();
+        formPom.pause(1000);
         formPom.clickPracticeForms();
+
         formPom.setFirstName(FIRST_NAME);
         formPom.setLastName(LAST_NAME);
         formPom.setEmail(EMAIL);
+        formPom.setGender(GENDER);
+        formPom.setNumber(NUMBER);
+        formPom.setDateOfBirth(DATE);
+        formPom.setSubject(SUBJECT);
+        formPom.setHobbies(HOBBY);
+        formPom.setState(STATE);
+        formPom.setCity(CITY);
+        formPom.clickSubmit();
+
+        String actualName = formPom.getTableData("Student Name");
+        Assert.assertEquals(actualName, FIRST_NAME + " " + LAST_NAME);
+
         System.out.println("---FINISH TEST---");
+        // formPom.pause(10000);
     }
 
     @AfterMethod
     public void afterMethod() {
         driver.quit();
     }
-
 }
